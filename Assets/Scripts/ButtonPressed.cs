d6379bd273cf7f89732f82e9a43933c955b25605
mashmouse <marli@remash.com>
2017-02-25 19:25:06 -0600

    Moving images from Images to Resources folder. Handler such that buttons change the image shown. Cleaned (a now seemingly unnecessary ButtonPRessed function.

---
+++ Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class ButtonPressed : MonoBehaviour {
+
+	public String startText;
+	public String outputText;
+
+	Text[] textContent;
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		textContent = GetComponentsInChildren<Text> ();
+		if (textContent != null && textContent.Length > 0){
+			text = textContent[0];
+			text.text = startText;
+			print("START: Text content of Button Exists as " + textContent[0].text + " (1) or not (0): " + textContent.Length);
+		} else { print("Button contains more than one element or no elements.");}
+	}
+
+
+	/*public void ButtonPressedResponse(){
+		if(this.text != null){
+			text.text = outputText;
+		} else {
+			print("Button text component not recognized.");
+		}
+	}*/
+}
---
+++ Assets/Scripts/Handler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Handler : MonoBehaviour {
+	//public String [] promptTexts;
+	//public String [] responseTexts;
+
+	public Image imageGo;
+
+	Button [] buttons;// I use Awake here so that the buttons are initialized through the ButtomWork scripts.
+
+	void Start () {
+		/*buttons = GetComponentsInChildren<Button> ();
+		if (buttons != null && buttons.Length == promptTexts.Length && buttons.Length == responseTexts.Length) {
+			for (int i = 0; i < buttons.Length; i++) {
+				print (buttons [i].name); // This is one nasty line of OO code. 
+				// It says fetch the ButtonWork object from the current button.
+				ButtonPressed bw = buttons [i].GetComponent<ButtonPressed> ();
+				print (bw);
+				//bw.startText = promptTexts [i];
+				//bw.outputText = responseTexts [i];
+			}
+		}else { 
+			Debug.Log ("This is why you don't use parallel arrays");
+		}*/
+	}
+
+	public void Option1Pressed(){
+		imageGo.sprite = Resources.Load<Sprite> ("spoon") as Sprite;
+	}
+
+	public void Option2Pressed(){
+		imageGo.sprite = Resources.Load<Sprite> ("dog");
+	}
+
+	public void Option3Pressed(){
+		imageGo.sprite = Resources.Load<Sprite> ("building");
+	}
+
+	public void Option4Pressed(){
+		imageGo.sprite = Resources.Load<Sprite> ("bicycle");
+	}
+}
